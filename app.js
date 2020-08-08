08e21793a4babd47ec8e8c07876c6b52765fa4ff
wolfseath <69370916+wolfseath@users.noreply.github.com>
2020-08-08 05:51:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAoixITMfVbkgSOhuMAZZhiCI0jcvEhi2M",
+  authDomain: "rudra-b01a9.firebaseapp.com",
+  databaseURL: "https://rudra-b01a9.firebaseio.com",
+  projectId: "rudra-b01a9",
+  storageBucket: "rudra-b01a9.appspot.com",
+  messagingSenderId: "635581046641",
+  appId: "1:635581046641:web:3d059658b1045c50fc60d0",
+  measurementId: "G-4V6QW1YBJT"
 };
 
      
