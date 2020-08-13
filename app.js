fa5217d667b66f21ca124706c0f5c095762bef93
HIDAYAT-hu <69606740+HIDAYAT-hu@users.noreply.github.com>
2020-08-13 16:49:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBdgt5M_An58QYY-pLrxEYM49wyk5rEckA",
+  authDomain: "ff-diamond-80ded.firebaseapp.com",
+  databaseURL: "https://ff-diamond-80ded.firebaseio.com",
+  projectId: "ff-diamond-80ded",
+  storageBucket: "ff-diamond-80ded.appspot.com",
+  messagingSenderId: "51726594233",
+  appId: "1:51726594233:web:dbf53fbbf3fbade9d42b68",
+  measurementId: "G-Y7GTMYH1V2"
 };
 
      
